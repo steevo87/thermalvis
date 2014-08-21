bf14f184a38f9f3286fbca807925021e83970e73
steevo87 <stephen.vidas@gmail.com>
2014-08-21 11:43:09 +0800

    Added missing new serial comms files.

---
+++ include/serial_comms.hpp
@@ -0,0 +1,80 @@
+/*! \file	serial_comms.hpp
+ *  \brief	Serial Comms information for the Miricle thermal cameras
+*/
+
+#ifndef _SERIAL_COMMS_H_
+#define _SERIAL_COMMS_H_
+
+#include "tools.hpp"
+
+#ifndef _WIN32
+#include <termios.h>
+#include <unistd.h>
+#include <sys/signal.h>
+#include <termio.h>
+#endif
+
+#include <stdio.h>
+
+#include <fcntl.h>
+
+#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#define BAUDRATE 								B38400
+#define MODEMDEVICE 							"/dev/ttyS0"
+#define _POSIX_SOURCE 							1         // POSIX compliant source
+#define FALSE 									0
+#define TRUE 									1
+#define SERIAL_BUFF_SIZE 						1024
+
+#define DEFAULT_BAUD_RATE						115200
+#define DEFAULT_SERIAL_COMMS_FORMAT				4
+#define DEFAULT_SERIAL_COMMS_PARITY				0
+#define DEFAULT_SERIAL_COMMS_STOP_BITS			1
+#define DEFAULT_SERIAL_COMMS_DATA_BITS			8
+
+#define MAX_VALID_THERMISTOR 					80.0
+#define MIN_VALID_THERMISTOR 				   -20.0
+#define MAX_TIME_WITHOUT_FLAGS 					45.0
+#define MAX_TIME_WITHOUT_NUC_conservative		45.0
+
+struct serialCommsDevice {
+	
+	volatile int STOP;
+
+	void signal_handler_IO (int status);    		// definition of signal handler
+	int wait_flag;                     		// TRUE while no signal received
+	char devicename[80];
+	long Baud_Rate;         				// default Baud Rate (110 through 38400)
+	long BAUD;                      				// derived baud rate from command line
+	long DATABITS;
+	long STOPBITS;
+	long PARITYON;
+	long PARITY;
+	int Data_Bits;              				// Number of data bits
+	int Stop_Bits;              				// Number of stop bits
+	int Parity;                 				// Parity as follows:
+													// 00 = NONE, 01 = Odd, 02 = Even, 03 = Mark, 04 = Space
+	int Format;
+	FILE *input;
+	FILE *output;
+	int status;
+
+	char message[90];
+
+	serialCommsDevice();
+
+};
+
+// Other stuff from: http://ubuntuforums.org/showthread.php?t=1395180
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h> 								// File control
+#include <errno.h> 								// Error number def
+
+void set_blocking (int fd, int should_block);
+
+#endif
---
+++ src/serial_comms.cpp
@@ -0,0 +1,61 @@
+/*! \file	serial_comms.cpp
+ *  \brief	Definitions for serial comms communications for Miricle cameras on Linux
+*/
+
+#include "serial_comms.hpp"
+
+serialCommsDevice::serialCommsDevice() :
+	STOP(FALSE),
+	wait_flag(TRUE),
+	Baud_Rate(DEFAULT_BAUD_RATE),
+	Data_Bits(DEFAULT_SERIAL_COMMS_DATA_BITS),
+	Stop_Bits(DEFAULT_SERIAL_COMMS_STOP_BITS),
+	Parity(DEFAULT_SERIAL_COMMS_PARITY),
+	Format(DEFAULT_SERIAL_COMMS_FORMAT) 
+{ }
+
+void set_blocking (int fd, int should_block)
+{
+        struct termios tty;
+        memset (&tty, 0, sizeof tty);
+        if (tcgetattr (fd, &tty) != 0)
+        {
+			ROS_ERROR("error %d getting term settings set_blocking", errno);
+			//return;
+        }
+
+        tty.c_cc[VMIN]  = should_block ? 1 : 0;
+        tty.c_cc[VTIME] = should_block ? 5 : 0; // 0.5 seconds read timeout
+
+        if (tcsetattr (fd, TCSANOW, &tty) != 0)
+                ROS_ERROR("error setting term %sblocking", should_block ? "" : "no");
+}
+
+void displayTermiosData(termios options) {
+	
+	ROS_INFO("   Termios Summary:");
+	
+	ROS_INFO("c_iflag = (%d)", options.c_iflag);
+	ROS_INFO("c_oflag = (%d)", options.c_oflag);
+	ROS_INFO("c_cflag = (%d)", options.c_cflag);
+	ROS_INFO("c_lflag = (%d)", options.c_lflag);
+	ROS_INFO("c_ispeed = (%d)", options.c_ispeed);
+	ROS_INFO("c_ospeed = (%d)", options.c_ospeed);
+	
+	for (unsigned int iii = 0; iii < NCCS; iii++) {
+		ROS_INFO("c_cc[%d] = (%d)", iii, options.c_cc[iii]);
+	}
+	
+	ROS_WARN("Termios Summary Complete.");
+	
+	/*
+	tcflag_t c_iflag;
+	tcflag_t c_oflag;
+	tcflag_t c_cflag;
+	tcflag_t c_lflag;
+	cc_t c_cc[NCCS];
+	speed_t c_ispeed;
+	speed_t c_ospeed
+	*/
+	
+}
